c741ee5cf885c9d8232c69016a03bbbf140bacb5
Léa Gris <lea.gris@noiraude.net>
2019-07-14 05:11:57 +0200

    Update convert_all.sh
    Comments added and typos corrected

--- examples/convert_all.sh
+++ examples/convert_all.sh
@@ -10,38 +10,58 @@
 
 source ../lcnumconv.sh
 
-sep_wc="$(locale numeric-thousands-sep-wc)"
+# Read some LC_NUMERIC locale settings for our own fancy use
+read -r -d '' _ thousands_sep _ _ sep_wc codeset < <(locale LC_NUMERIC)
 
-if ((sep_wc<255)); then
-  sep_unit_value="$(printf 'ASCII %d' "${sep_wc}")"
+# Compose the character-set and the code value of the thausands separator
+if ((sep_wc < 255)); then
+  sep_unit_value="$(printf '%s %d' "${codeset}" "${sep_wc}")"
 else
-  sep_unit_value="$(printf 'Unicode U+%04X' "${sep_wc}")"
+  sep_unit_value="$(printf '%s U+%04X' "${codeset}" "${sep_wc}")"
 fi
 
+# Print detailed informations about the current LC_NUMERIC locale settings
 cat <<EOF
-Your current numeric locale format is:
+The current numeric locale setting is:
 LC_NUMERIC=${LC_NUMERIC}
 
-The systems defines the folloiwng numeric format settings:
+This host defines the folloiwng numeric format settings for the ${LC_NUMERIC} locale:
 $(locale -k LC_NUMERIC)
 
-You can group and separate digits accordingly
-to the '$(locale thousands_sep)' character, wich is
-also known as: ${sep_unit_value}.
+Digits groups may be separated by the '${thousands_sep}' character,
+wich is also known as: ${sep_unit_value}.
+
 EOF
 
-read -r -p "Now please enter floating-point a number in your ${LC_NUMERIC} format:"$'\n' locale_float
+# Ask for a floating-point number formatted in current locale
+read -r -p "Please enter a floating-point number in your ${LC_NUMERIC} format:"$'\n' locale_float
 
-printf '\n%-31s %-40s\n' 'Locales known to host' 'Number formatted to locale'
-hl="$(printf '%-72s' '')"; printf '%s\n' "${hl// /-}"
+# Print a table of host locales and number formatted to these locales
+
+# Columns headers
+printf '\n\n%-31s %-40s\n' 'Locales known to host' 'Number formatted to locale'
+
+# Make string of 72 spaces
+hl="$(printf '%-72s' '')"
+
+# Print a line of 72 dashes
+printf '%s\n' "${hl// /-}"
+
+# Iterate all host's known locales except the current numeric locale
 for to_lc in $(
-  locale --all | \
-    grep \
+  locale --all-locales \
+    | grep \
       --invert-match \
       "${LC_NUMERIC}"
 ); do
+  # Transform the current locale formatted number into the destination locale
+  # using the lcnumconv.sh library's l2l (locale to locale) method
   translated_float="$(
     lcnumconv::l2l "${LC_NUMERIC}" "${to_lc}" "${locale_float}"
   )"
+
+  # Switch the numeric locale to the destination locale
+  # to allow the Bash's built-in printf to accept this locale's format
+  # Print the table's line with host's locale and formatted number
   LC_NUMERIC="${to_lc}" printf '%-31s %f\n' "${to_lc}" "${translated_float}"
 done
